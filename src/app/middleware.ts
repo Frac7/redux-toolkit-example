a539da6512ffb8d836f9cbbf55cbcfef26be2a07
Frac7 <francescacella96@gmail.com>
2023-02-24 11:44:40 +0100

    Add toast for reques status in mw

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,4 +1,4 @@
-import { isRejectedWithValue } from "@reduxjs/toolkit";
+import { isFulfilled, isPending, isRejectedWithValue } from "@reduxjs/toolkit";
 import type { MiddlewareAPI, Middleware } from "@reduxjs/toolkit";
 import { message } from "antd";
 
@@ -10,6 +10,14 @@ export const rtkQueryErrorLogger: Middleware =
     // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
     if (isRejectedWithValue(action)) {
       message.error(`${action.payload.status}: ${action.error.message}`);
+    }
+
+    if (isPending(action)) {
+      message.info(action.meta.requestStatus.toUpperCase());
+    }
+
+    if (isFulfilled(action)) {
+      message.success(action.meta.requestStatus.toUpperCase());
     }
 
     return next(action);
